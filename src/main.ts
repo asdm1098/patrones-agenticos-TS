570f1b08400098c1e2800866f38930121899561c
asdm1098 <asdm1098@gmail.com>
2026-09-19 21:36:50 +0200

    add planing patter

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import './helpers/string-colors.js';
-import { toolUseMain } from './patterns/01-tool-use/tool-use.js';
+import { planningMain } from './patterns/02-planning/planning.js';
+// import { toolUseMain } from './patterns/01-tool-use/tool-use.js';
 // import { /* getMessageFromModel, */ getMessageFromModelFailSafe } from './actions/get-message-model.js';
 
 
 console.clear();
 
 // await getMessageFromModelFailSafe();
-await toolUseMain();
+// await toolUseMain();
+await planningMain();
